9222aec4a616e6de12a8563707aef45fdaa3bbfa
Ashen Shanuka <shanuka-im19008@stu.kln.ac.lk>
2023-02-18 17:59:30 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizzapp/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.quizzapp;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+            "Which company owns the Android?",
+            "Which One is not a programming Language?",
+            "Where you are watching this video?",
+            "Which Company Owns Assassin's Creed?"
+    };
+
+    public static String Choices[][]={
+            {"Google","Apple","Nokia","Samsung"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Facebook","Whatsapp","Instagram","Youtube"},
+            {"Activition","Rockstar","Ubisoft","Epic"}
+
+
+    };
+
+    public static String correctAnswers[]={
+            "Google",
+            "Notepad",
+            "Youtube",
+            "Ubisoft"
+
+
+    };
+}
